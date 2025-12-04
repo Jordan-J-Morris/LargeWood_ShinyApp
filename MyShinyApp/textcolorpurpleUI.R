d26470ec8fe064c42053b53efc6a86bd6a69c190
Jordan-J-Morris <jm1731@exeter.ac.uk>
2025-12-04 17:02:14 +0000

    Delete MyShinyApp/textcolorpurpleUI.R

--- MyShinyApp/textcolorpurpleUI.R
+++
@@ -1,9 +0,0 @@
-# ---- UI.R: Define UI for LW Visualisation Application ----
-
-library(shiny)
-library(leaflet)
-
-ui <- fluidPage(
-  titlePanel("Large Wood Cluster Visualization and Analysis"),
-  leafletOutput("map", height = "800px")
-)
